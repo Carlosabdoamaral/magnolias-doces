18bc5f0478512346723640600a5b3b77421fa085
Carlosabdoamaral <72256470+Carlosabdoamaral@users.noreply.github.com>
2022-03-14 23:52:39 -0300

    Ajustes no index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,14 +4,12 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from "react-router-dom";
 import Urls from "./routes";
 import './index.scss'
+import IndexView from "./views/Index/IndexView";
 ReactDOM.render(
-    <BrowserRouter>
-        <Urls/>
-    </BrowserRouter>,
+    <main>
+        <IndexView/>
+    </main>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
