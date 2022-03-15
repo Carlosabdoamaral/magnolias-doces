ba1e635d4494b8c552ed5afcc405f519c0b4d320
Carlosabdoamaral <72256470+Carlosabdoamaral@users.noreply.github.com>
2022-03-14 23:59:15 -0300

    Ajustes no index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
-// import {BrowserRouter} from "react-router-dom";
-// import Urls from "./routes";
+import {BrowserRouter} from "react-router-dom";
+import Urls from "./routes";
 import './index.scss'
-import IndexView from "./views/Index/IndexView";
 ReactDOM.render(
-    <main>
-        <IndexView/>
-    </main>,
+    <BrowserRouter>
+        <Urls/>
+    </BrowserRouter>,
   document.getElementById('root')
 );
 
